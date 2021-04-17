f73f40774a423715e3f1fb69af0efcdc6e0aee74
surmount96 <ayanwoye74@gmail.com>
2021-04-17 12:04:54 +0100

    migrations files

---
+++ config/modules-auth-form-fields.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+  'fields' => [
+    [
+      'field_name' => 'profession',
+      'field_type' => 'enum',
+      'enum_data' => [
+        'developer',
+        'designer',
+        'project management'
+      ],
+    ],
+
+  [
+      'field_name' => 'Framework',
+      'field_type' => 'enum',
+      'enum_data' => [
+          'laravel',
+          'Vuejs',
+          'react'
+      ],
+  ],
+
+    
+
+  ]
+];
--- config/modules-auth.php
+++ config/modules-auth.php
@@ -1,10 +1,12 @@
 <?php
 
 return [
-  'title' => 'Auth',
-  'view' => [
-    'company_name' => env('MODULLO_COMPANY_NAME','Modullo'),
-    'allow_registration' => env('MODULLO_ALLOW_REGISTRATION',true),
-      'company_logo' => env('MODULLO_COMPANY_LOGO')
-  ]
+    'title' => 'Auth',
+    'view' => [
+        'company_name' => env('MODULLO_COMPANY_NAME','Modullo'),
+        'allow_registration' => env('MODULLO_ALLOW_REGISTRATION',true),
+        'company_logo' => env('MODULLO_COMPANY_LOGO'),
+        'custom_form_fields' => true
+
+    ]
 ];

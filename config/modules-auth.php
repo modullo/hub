1814b66c7b9661890be8ac52eca1c3e0911c598b
tomideo1 <ayotomideaina@gmail.com>
2021-05-27 12:03:19 +0100

    local changes

--- config/modules-auth.php
+++ config/modules-auth.php
@@ -5,6 +5,8 @@ return [
   'view' => [
     'company_name' => env('MODULLO_COMPANY_NAME','Modullo'),
     'allow_registration' => env('MODULLO_ALLOW_REGISTRATION',true),
-      'company_logo' => env('MODULLO_COMPANY_LOGO')
+    'company_logo' => env('MODULLO_COMPANY_LOGO'),
+    'custom_form_fields' => true
+
   ]
 ];
